c805e6aadab8bcfbd2b401a07a100490e01c141b
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2020-03-15 12:43:29 +0200

    Catch errors in main loop

--- src/index.ts
+++ src/index.ts
@@ -16,26 +16,30 @@ client.on('ready', () => {
 client.on('message', handleMessage);
 
 const main = async (): Promise<void> => {
-  // Get old data from storage, so it can be compared with new API response
-  let oldData = storage.getStore();
+  try {
+    // Get old data from storage, so it can be compared with new API response
+    let oldData = storage.getStore();
 
-  // If apiData is null, it usually means the program has just started, let's
-  // try to load data from file in that case.
-  if (oldData.apiData === null) {
-    await storage.loadStorageDataFromFile();
-    oldData = storage.getStore();
+    // If apiData is null, it usually means the program has just started, let's
+    // try to load data from file in that case.
+    if (oldData.apiData === null) {
+      await storage.loadStorageDataFromFile();
+      oldData = storage.getStore();
+    }
+
+    const apiResponse = await fetch(
+      'https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData',
+    );
+    const result: HSApiResponse = await apiResponse.json();
+
+    const counts = getTotalCounts(result);
+    const stats = parseStatisticsFromData(result);
+
+    // Update new data to storage
+    await storage.updateAPIData(counts, stats);
+  } catch (error) {
+    console.error('Error in main loop: ' + error);
   }
-
-  const apiResponse = await fetch(
-    'https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData',
-  );
-  const result: HSApiResponse = await apiResponse.json();
-
-  const counts = getTotalCounts(result);
-  const stats = parseStatisticsFromData(result);
-
-  // Update new data to storage
-  await storage.updateAPIData(counts, stats);
 
   setTimeout(main, config.apiPollInterval);
 };
